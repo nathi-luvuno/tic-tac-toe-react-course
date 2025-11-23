5ff14bf7f0fee287ab3ebb13477b02025a0c4b32
Nkosinathi Luvuno <nkosinathi.luvuno1@gmail.com>
2025-11-23 19:16:53 +0200

    deployment bug and added playlist

--- src/contexts/ModalContext.js
+++ src/contexts/ModalContext.js
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { createContext } from "react";
 import { useModal } from "../hooks/useModal";
 import ModalTemplate from "../components/Modal/ModalTemplate";
 
